49abdc06582ba7250072bb7a89cbd3c5b8e9febf
Roan Carpenter <roan.carpenter@mrjaffesclass.com>
2023-01-03 14:20:08 -0800

    1

---
+++ CarpenterRoan.java
@@ -0,0 +1,31 @@
+
+/**
+ * A BoopleSnoot is Mr. Jaffe's animal
+ * 
+ * @author Mr. Jaffe
+ * @version 1.0 2017-07-14
+ */
+public class CarpenterRoan extends Animal
+{
+   
+  public CarpenterRoan() {
+    super("Roan Carpenter", "Blue", 2);
+  }
+  
+  public String getType() {
+    return "Blastoise";
+  }
+  
+  public String speak() {
+    return "Blast";
+  }
+  
+  public String getDisposition() {
+    return "Could have an oran berry right about now";
+  } 
+  
+  public String getName() {
+    return "Blastoise";
+  } 
+  
+}
